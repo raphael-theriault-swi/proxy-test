89ab1c8cfc10681cd5e04deaccfe696db569aefe
Raphaël Thériault <raphael.theriault@solarwinds.com>
2023-08-28 11:53:40 -0700

    stuff

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const http = require("node:http");
+const why = require("why-is-node-running");
+
+switch (process.argv[2]) {
+  case "new": {
+    require("solarwinds-apm-new").waitUntilAgentReady(10_000);
+    break;
+  }
+  case "old": {
+    require("solarwinds-apm-old").readyToSample(10_000);
+    break;
+  }
+}
 
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
   res.writeHead(200, { "content-type": "text/plain" });
   res.end("Hello, World!");
 });
-server.listen(0);
+server.listen(0, "localhost");
 
 server.on("listening", () => {
   const { address, port, family } = server.address();
@@ -17,22 +29,21 @@ server.on("listening", () => {
   const get = () =>
     new Promise((res, rej) => {
       let data = "";
-      http
-        .get(target, (r) =>
-          r
-            .setEncoding("utf-8")
-            .on("data", (chunk) => (data += chunk))
-            .on("end", () => {
-              console.log(data);
-              res();
-            })
-        )
-        .on("error", rej);
+      const req = http.get(target, (r) =>
+        r
+          .setEncoding("utf-8")
+          .on("data", (chunk) => (data += chunk))
+          .on("end", () => {
+            console.log(data);
+            res();
+          })
+      );
+      req.on("error", rej);
     });
 
   const reqs = [];
   for (let i = 0; i < 10; i++) {
     reqs.push(get());
   }
-  Promise.all(reqs).then(() => server.close());
+  Promise.all(reqs).then(() => server.close(() => why()));
 });
